3b8e281fa0d234945e2aa632da60b226dd659cf1
Надя <207410775+nadiantnk@users.noreply.github.com>
2026-09-23 20:43:56 +0700

    Update schema.sql

--- database/schema.sql
+++ database/schema.sql
@@ -1,9 +1,13 @@
 CREATE TABLE specializations (
-    ...
+    specialization_id INT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL UNIQUE
 );
 
 CREATE TABLE doctors (
-    ...
+    doctor_id SERIAL PRIMARY KEY
+    first_name VARCHAR(100) NOT NULL
+    last_name VARCHAR(100) NOT NULL
+    specialization_id NUM FOREIGN KEY
 );
 
 CREATE TABLE patients (
