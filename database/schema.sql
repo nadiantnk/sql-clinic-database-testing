dfe9185027631862de1947affaf3e61e917cf599
Надя <207410775+nadiantnk@users.noreply.github.com>
2026-09-26 17:32:24 +0700

    Update schema.sql

--- database/schema.sql
+++ database/schema.sql
@@ -1,5 +1,5 @@
 CREATE TABLE specializations (
-    specialization_id INT PRIMARY KEY,
+    specialization_id SERIAL PRIMARY KEY,
     name VARCHAR(100) NOT NULL UNIQUE
 );
 
@@ -7,29 +7,36 @@ CREATE TABLE doctors (
     doctor_id SERIAL PRIMARY KEY,
     first_name VARCHAR(100) NOT NULL,
     last_name VARCHAR(100) NOT NULL,
-    specialization_id INT,
+    specialization_id INT NOT NULL,
     FOREIGN KEY (specialization_id)
         REFERENCES specializations(specialization_id)
+        ON DELETE RESTRICT
 );
 
 CREATE TABLE patients (
-    patient_id INT PRIMARY KEY,
-    first_name VARCHAR(100),
-    last_name VARCHAR(100),
+    patient_id SERIAL PRIMARY KEY,
+    first_name VARCHAR(100) NOT NULL,
+    last_name VARCHAR(100) NOT NULL,
     patronymic VARCHAR(100),
-    date_of_birth DATE,
+    date_of_birth DATE NOT NULL,
     phone VARCHAR(20),
-    gender CHAR(1) CHECK (gender IN ('М', 'Ж')),
-    policy_number VARCHAR(50) UNIQUE
+    gender CHAR(1)
+        CHECK (gender IN ('М', 'Ж')),
+    policy_number VARCHAR(50) NOT NULL UNIQUE
 );
 
 CREATE TABLE appointments (
-    appointment_id INT,
-    patient_id INT,
-    doctor_id INT,
-    appointment_date DATE,
-    appointment_time TIME,
-    status VARCHAR(20),
+    appointment_id SERIAL PRIMARY KEY,
+    patient_id INT NOT NULL,
+    doctor_id INT NOT NULL,
+    appointment_date DATE NOT NULL,
+    appointment_time TIME NOT NULL,
+    status VARCHAR(20) NOT NULL
+        CHECK (status IN ('Запланирован', 'Завершен', 'Отменен', 'Не явился')),
     FOREIGN KEY (patient_id)
-       REFERENCES patients(patient_id)
+        REFERENCES patients(patient_id)
+        ON DELETE RESTRICT,
+    FOREIGN KEY (doctor_id)
+        REFERENCES doctors(doctor_id)
+        ON DELETE RESTRICT
 );
